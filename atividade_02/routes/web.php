93dcc20a7b52605772d9ef20c00301b1c7a90b12
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-05-30 23:09:55 -0300

    Inicia prática 5.2

--- atividade_02/routes/web.php
+++ atividade_02/routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 
+// Rota da página inicial
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Rotas de autenticação
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Rota da dashboard
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Rota para o CRUD de categorias
+Route::resource('categories', CategoryController::class);
